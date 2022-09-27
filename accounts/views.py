68c8b4457865771da212861269a5aadce9e0dde0
nisargs <nisarg2198@gmail.com>
2022-09-27 09:50:36 +0530

    added message alerts

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        messages.error(request, 'TEsting')
+        return redirect('register')
+        pass
+    else:
+        return render(request, 'accounts/register.html')
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        #register user
+        pass
+    else:
+        return render(request, 'accounts/login.html')
+    
 
 def logout(request):
     return redirect('index')
