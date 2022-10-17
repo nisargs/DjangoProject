0c538191e2096412f74fc8f7d0a746f5a83f6278
nisargs <nisarg2198@gmail.com>
2022-10-17 11:20:49 +0530

    Dashboard Functionality

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from functools import reduce
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
+from contacts.models import Contact
 
 # Create your views here.
 def register(request):
@@ -64,4 +65,9 @@ def logout(request):
         return redirect('index')
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+    
+    context = {
+        'contacts': user_contacts
+    }
+    return render(request, 'accounts/dashboard.html', context)
